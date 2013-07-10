fdf06e9610df330b6d2dbf5fc18acf11dabb434e
Pavo <pavo@ubuntu.ubuntu-domain>
2013-07-10 05:36:57 +0200

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
+var content = fs.readFileSync("~/bitstarter/index.html", "utf-8");
 
+var buffer = new Buffer(256);
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString("utf-8", content));
 });
 
 var port = process.env.PORT || 5000;
